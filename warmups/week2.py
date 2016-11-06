a662cb230cb2054e27b860018ce6003196e0ef8a
Lorenzo Brown <lrnzbr@gmail.com>
2016-11-06 12:05:41 +0200

    Create week2.py

---
+++ warmups/week2.py
@@ -0,0 +1,16 @@
+def shift_on_character(string, char):
+    '''
+    INPUT: string, string
+    OUTPUT: string
+    
+    Download this week2.py file and modify it in your favorite IDE (Idle, Sublime, ipython notebook etc.)
+    
+    Find the first occurence of the character char and return the string witth
+    everything before char moved to the end of the string. If char doesn't
+    appear, return the same string.
+    
+    Example:
+    >>> shift_on_character("elephant", "p")
+    'phantele'
+    '''
+    pass  #remove this line and add a return statement when you are finished
