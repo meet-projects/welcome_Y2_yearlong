106aa620c02c7a3715ac8faea865c4e80aa09464
Lorenzo Brown <lrnzbr@gmail.com>
2016-11-06 12:07:34 +0200

    Update week2.py

--- warmups/week2.py
+++ warmups/week2.py
@@ -5,7 +5,7 @@ def shift_on_character(string, char):
     
     Download this week2.py file and modify it in your favorite IDE (Idle, Sublime, ipython notebook etc.)
     
-    Find the first occurence of the character char and return the string witth
+    Find the first occurence of the character char and return the string with
     everything before char moved to the end of the string. If char doesn't
     appear, return the same string.
     
