6b1fbf12779eabdd58c18a26f2bafb6350bdffcd
Lorenzo Brown <lrnzbr@gmail.com>
2016-11-13 08:55:52 +0200

    Create week3.py

---
+++ warmups/week3.py
@@ -0,0 +1,15 @@
+def count_characters(string):
+    '''
+    INPUT: STRING
+    OUTPUT: DICT (STRING => INT)
+
+    Return a dictionary which contains a count of the number of times each
+    character appears in the string, case-insensitive ("A" and "a" should be treated as the same character).
+    Characters which would have a count of 0 should not need to be included in
+    your dictionary.
+    
+    Example:  count_characters("Good Morning")
+              >>>{'g':3, 'o':3, 'd': 1, "m":1, "r":1, "n":2, "i":1}
+    
+    '''
+    pass
