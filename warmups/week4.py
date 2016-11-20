b46d7d1f38a5dd0f47d0507e394b2d4935172632
Lorenzo Brown <lrnzbr@gmail.com>
2016-11-20 14:20:43 +0200

    Create week4.py

---
+++ warmups/week4.py
@@ -0,0 +1,29 @@
+``` Read the python documentation on Counters as reference to do the warmup 
+
+https://docs.python.org/3/library/collections.html#collections.Counter
+
+```
+
+from collections import Counter
+def most_common_letters(sentence):
+    """
+    INPUT: string
+    OUTPUT: list of strings
+
+    Given a sentence, give the most common letter for each word.
+    You should lowercase the letters. If there's a tie, include any of them.
+
+    example:
+    INPUT: "If you had a twin, I would still choose you"
+    OUTPUT: 'f u h a , i d l o u'
+
+
+    Use Counter and the string join method
+  
+    """
+    answer_string = ""
+    
+    #YOUR CODE GOES HERE
+    
+  
+    return answer_string.strip()
