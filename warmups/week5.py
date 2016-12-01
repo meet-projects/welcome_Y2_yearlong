f1294496860f95bac7a7eb27e270fd5215e1c434
Lorenzo Brown <lrnzbr@gmail.com>
2016-12-01 17:14:18 +0200

    Update week5.py

--- warmups/week5.py
+++ warmups/week5.py
@@ -1,8 +1,8 @@
 '''
 You've built an in-flight entertainment system with on-demand movie streaming.
-Users on longer flights like to start a second movie right when their first one ends, but they complain that the plane usually lands before they can see the ending. So you're building a feature for choosing two movies whose total runtimes will equal the exact flight length.
+Users on longer flights like to start a second movie right when their first one ends, but they complain that the plane usually lands before they can see the ending. So you're building a feature for choosing two movies whose total runtimes will equal less than the flight length.
 
-Write a function that takes an integer flight_length (in minutes) and a list of integers movie_lengths (in minutes) and returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length.
+Write a function that takes an integer flight_length (in minutes) and a list of integers movie_lengths (in minutes) and returns a list of tuples containing movie pairs the flight attendents can show along with the total time the movies will take to run. 
 
 When building your function:
 
@@ -11,7 +11,7 @@ When building your function:
 -Optimize for runtime over memory (extra credit)
 '''
 flight_length = 250
-movie_lengths = [90,145,185]
+movie_lengths = {"Free Willie" : 90, "Finding Nemo" : 145, "Pirates of the Caribbean": 185, "Interstellar": 65} 
 
 def in_flight_movies(flight_length, movie_lengths):
   #YOUR CODE HERE
