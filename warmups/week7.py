5ee91a9df473dcc1b9b592b7447497ad32b6f641
Lorenzo Brown <lrnzbr@gmail.com>
2016-12-11 15:16:38 +0200

    Create week7.py

---
+++ warmups/week7.py
@@ -0,0 +1,9 @@
+isAPalindrome(sentence):
+'''This function should test of a sentence is a palindrome (spelled the same way forwards and backwards).  HINT: Use recursion to solve this problem'''
+
+
+pass
+
+isAPalindrome("Abba")
+isAPalindrome("I Have a puppy ")
+isAPalindrome("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!")
